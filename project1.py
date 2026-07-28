34c705ece3709d7ed632a645d9fa8fd1f6757802
vaidehiib29@gmail.com <vaidehiberani@gmail.com>
2026-07-28 12:27:51 +0530

    fundamentalbooster

--- project1.py
+++ project1.py
@@ -4,7 +4,7 @@ name = input("Please Enter Your Name: ")
 age = int(input("Please Enter Your Age: "))
 height = float(input("Please Enter Height in meters: "))
 favourite_number = int(input("Please Enter Your favourite number: "))
-
+ 
 birth_year = 2026-age
 
 print("\n Thank you! Here is the information we collected:\n")
@@ -16,7 +16,7 @@ print("Favourite Number :",favourite_number,"(Type :",type(favourite_number),",M
 
 height_as_int=int(height)
 
-print("\n Type converstion demostraction :")
+print("\n Type converstion demostraction :") 
 
 print("Original height:",height,"(flot)")
 print("Converted height:",height_as_int,"(int)")
