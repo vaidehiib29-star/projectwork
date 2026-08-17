7b9568e4084cf01a3bb8248ae2745952b4118577
vaidehiib29@gmail.com <vaidehiberani@gmail.com>
2026-08-17 19:21:37 +0530

    EmployeeManagementSystem

--- Pythonprojectwork/project2.py
+++ Pythonprojectwork/project2.py
@@ -5,8 +5,8 @@ while True:
     2.analyze a range of numbers
     3.exit""")
     a=int(input("Enter the option: "))
-    if a==1:
-        userinput=int(input("Enter the number of rows for the pattern :"))
+    if a == 1:
+        userinput = int(input("Enter the number of rows for the pattern :"))
         for i in range(0,userinput):
             for j in range(i):
                 print("*",end="")
@@ -16,17 +16,26 @@ while True:
         q=int(input("Enter the end of the range: "))
         sum=0
         for i in range(p,q+1):
-            sum+=i
-            if i%2==0:
+            sum += i
+            if i % 2 == 0:
                 print(f"{i} is an even number")
             else:
                 print(f"{i} is an odd number")
         print(f"Sum of all numbers from {p} to {q} is {sum}")
-    elif     a==3:
+    elif a == 3:
         print("Exiting the program ,goodbye!")
         break
     else:
         print("invalid option")
 
 
-    
+
+        while True:
+            print("Welcome to the pattern genratir and analyzer")
+            print("""select the option
+            1.Genrate a pattern
+            2.analyze a range of numbers3
+            3.exit""")
+
+
+        
---
+++ Pythonprojectwork/project5.py
@@ -0,0 +1,121 @@
+class Employee:
+    def __init__(self, id, name, salary):
+        self.id = id
+        self.name = name
+        self.__salary = salary
+
+    def get_salary(self):
+        return self.__salary
+
+    def set_salary(self, salary):
+        if salary < 0:
+            print("Salary can not be negative")
+        else:
+            self.__salary = salary
+            print("salary updated")
+
+    def display(self):
+        print("ID:", self.id)
+        print("Name:", self.name)
+        print("Salary:", self.__salary)
+
+class Developer(Employee):
+
+    def __init__(self, id, name, salary, language, experience):
+        super().__init__(id, name, salary)
+
+        self.language = language
+        self.experience = experience
+
+    def display(self):
+        super().display()
+        print("Language:", self.language)
+
+
+class Developer(Employee):
+
+    def __init__(self, id, name, salary, language, experience):
+        super().__init__(id, name, salary)
+        self.language = language
+        self.experience = experience
+
+    def display(self):
+        super().display()
+        print("Language:", self.language)
+        print("Experience:", self.experience)
+
+
+class Manager(Employee):
+    def __init__(self, id, name, salary, department, team):
+        super().__init__(id, name, salary)
+        self.department = department
+        self.team = team
+
+    def display(self):
+        super().display()
+        print("Department:", self.department)
+        print("Team Size:", self.team)
+
+
+class TechnicalSkills:
+
+    def __init__(self, skill, certificate):
+        self.skill = skill
+        self.certificate = certificate
+
+    def show_skill(self):
+        print("Skill:", self.skill)
+        print("Certificate:", self.certificate)
+
+
+class TechLead(Developer, TechnicalSkills):
+
+    def __init__(self, id, name, salary, language, experience, skill, certificate):
+
+        Developer.__init__(self, id, name, salary, language, experience)
+
+        TechnicalSkills.__init__(self, skill, certificate)
+
+    def display(self):
+        Developer.display(self)
+        self.show_skill()
+
+
+#-----Employee------
+print("\nEmployee Details")
+
+e1 = Employee(1, "vaidehi", 30000)
+e1.display()
+
+#------Developer-----
+print("\nDeveloper Details")
+
+d1 = Developer(2, "Jay", 45000, "Python", 2)
+d1.display()
+
+#------Manager------
+print("\nManager Details")
+
+m1 = Manager(3, "Het", 60000, "IT", 3)
+m1.display()
+
+#----Tech-Lead----
+print("\nTech Lead Details")
+
+t1 = TechLead(4, "Shivu", 40000, "Python", 5, "Cloud", "AWS")
+t1.display()
+
+#------Getter-----
+print("\nSalary using Getter")
+print(e1.get_salary())
+
+#-------Setter----
+print("\nChanging Salary")
+e1.set_salary(35000)
+print("New Salary:", e1.get_salary())
+
+#-----Negative Slary----
+print("\nTesting Negative Salary")
+e1.set_salary(-10000)
+        
+                
